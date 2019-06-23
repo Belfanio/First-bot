56575b3ade8ddef8be87cb97e58ce9898f4be8e1
Sergey <ulitin_sergey@mail.ru>
2019-06-23 13:18:27 +0200

    Parsing bot

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
 import requests
+import re
 import variables as var
+import bs4
 
 
 
@@ -8,12 +10,40 @@ import variables as var
 bot = telebot.TeleBot(var.bot_token)
 
 def get_kurs_onDate_usd(Date):
-    response = requests.get(var.bank_url_usd.format(Date))
-    return response.json()
+    try:
+        response = requests.get(var.bank_url_usd.format(Date))
+        if response.status_code == 200:
+            return ('Подключение установлено')
+        elif response.status_code == 404:
+            return ('Страница не найдена')
+        elif response.status_code == 500:
+            return ('Внутренняя ошибка сервера')
+        else:
+            return ('Иная ошибка')
+    except ConnectionError:
+        return ('Ошибка подключения')
+    except requests.exceptions.InvalidSchema:
+        return ('Ошибка В URL')
+    finally:
+        return response.json()
 
 def get_kurs_onDate_euro(Date):
-    response = requests.get(var.bank_url_euro.format(Date))
-    return response.json()
+    try:
+        response = requests.get(var.bank_url_euro.format(Date))
+        if response.status_code == 200:
+            return ('Подключение установлено')
+        elif response.status_code == 404:
+            return ('Страница не найдена')
+        elif response.status_code == 500:
+            return ('Внутренняя ошибка сервера')
+        else:
+            return ('Иная ошибка')
+    except ConnectionError:
+        return ('Ошибка подключения')
+    except requests.exceptions.InvalidSchema:
+        return ('Ошибка В URL')
+    finally:
+        return response.json()
 
 def get_kurs_usd(json_answer): 
     Cur_Rate = json_answer['Cur_OfficialRate']
@@ -24,8 +54,22 @@ def get_kurs_euro(json_answer):
     return Cur_Rate
 
 def weather():
-    response = requests.get(var.weather_url)
-    return response.json()
+    try:
+        response = requests.get(var.weather_url)
+        if response.status_code == 200:
+            return ('Подключение установлено')
+        elif response.status_code == 404:
+            return ('Страница не найдена')
+        elif response.status_code == 500:
+            return ('Внутренняя ошибка сервера')
+        else:
+            return ('Иная ошибка')
+    except ConnectionError:
+        return ('Ошибка подключения')
+    except requests.exceptions.InvalidSchema:
+        return ('Ошибка В URL')
+    finally:
+        return response.json()
 
 def temperature_min(json_answer):
     minimum = json_answer['main']['temp_min']
@@ -34,6 +78,27 @@ def temperature_min(json_answer):
 def temperature_max(json_answer):
     maximum = json_answer['main']['temp_max']
     return maximum
+
+def get_from_habr():
+    response = requests.get(var.habr_url)
+    return response.text
+
+def get_pars():
+    text = bs4.BeautifulSoup(get_from_habr(), 'html.parser')
+    return text
+
+def Title():
+    title = get_pars().select('a.post__title_link')
+    length = len(title)
+    i = 0
+    New_title = []
+    while i < length:
+        text = str(title[i])
+        New_title.append(re.sub('<[^>]*>', '', text))
+        i += 1
+    return New_title
+
+
 
 @bot.message_handler(content_types=["text"])
 def send_message(message):
@@ -44,6 +109,13 @@ def send_message(message):
         minimum = temperature_min(weather())
         maximum = temperature_max(weather())
         bot.send_message(message.chat.id, f'Температура на сегодня: минимальная - {minimum}, максимальная - {maximum}')
+    elif message.text.lower() == 'хабр':
+        i = 0
+        length = len(Title())
+        title = Title()
+        while i < length:
+            bot.send_message(message.chat.id, title[i])
+            i += 1
     else:
         Date = message.text
         USD = get_kurs_usd(get_kurs_onDate_usd(Date))
--- variables.py
+++ variables.py
@@ -4,3 +4,4 @@ bank_url_usd = 'http://www.nbrb.by/API/ExRates/Rates/145?onDate={}'
 bank_url_euro = 'http://www.nbrb.by/API/ExRates/Rates/292?onDate={}'
 weather_url = 'https://openweathermap.org/data/2.5/weather?id=625665&appid=b6907d289e10d714a6e88b30761fae22'
 methods = {'updates': 'GetUpdates', 'send':'sendMessage'}
+habr_url = 'https://habr.com/ru/all/'
